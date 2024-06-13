4294c3937a59f36374c084cb6373308e0524d544
Chandana <101915079+chandana38@users.noreply.github.com>
2024-06-13 23:09:54 +0530

    Create write_back.v

---
+++ src/write_back.v
@@ -0,0 +1,18 @@
+module write_back(input [31:0] alu_output,
+                  input clk,
+                  input rst,
+                  input [31:0] instr_reg_fetch,
+                  input [31:0] loaded_value,
+                  input [31:0] cntrl_sig_decode,
+                  output reg [31:0] regs);
+  
+  always @(posedge clk)
+    begin
+      if (cntrl_sig_decode == 32'hC0F) begin 
+        instr_reg_fetch[11:7] = loaded_value;
+        
+      end
+      else instr_reg_fetch[11:7] = alu_output;
+
+    end
+endmodule
